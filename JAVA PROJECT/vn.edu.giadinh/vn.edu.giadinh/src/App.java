938ae841546739c85c68128ce8aa58118cace27d
hoang304 <hoangyor304@gmail.com>
2021-09-20 13:21:38 +0700

    vừa cập nhật

--- JAVA PROJECT/vn.edu.giadinh/vn.edu.giadinh/src/App.java
+++ JAVA PROJECT/vn.edu.giadinh/vn.edu.giadinh/src/App.java
@@ -1,5 +1,5 @@
-public class program {
+public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("hello program");
+        System.out.println("Hello, World!");
     }
 }
--- JAVA PROJECT/vn.edu.giadinhh/src/App.java
+++ JAVA PROJECT/vn.edu.giadinhh/src/App.java
@@ -1,4 +1,3 @@
-package vn.edu.giadinh.baitap.lap1;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
